99f7b76f76f8dce4d9edc044ec7b36d3097dae87
Abhierror <abhialmalcool@gmail.com>
2026-07-04 12:00:16 +0530

    feat(Categories): Added Categories features

--- main/urls.py
+++ main/urls.py
@@ -3,5 +3,10 @@ from . import views
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
     path('login/', views.login_view, name='login'),
-    path('logout/', views.logout_view, name='logout')
+    path('logout/', views.logout_view, name='logout'), 
+
+    path('categories-list/', views.CategoryListView.as_view(), name="category_list"),
+    path('categories/add/', views.CategoryCreateView.as_view(), name="category_add"),
+    path('categories/<int:pk>/edit', views.CategoryUpdateView.as_view(), name="category_edit"),
+    path('categories/<int:pk>/delete', views.CategoryDeleteView.as_view(), name="category_delete"), 
 ]
